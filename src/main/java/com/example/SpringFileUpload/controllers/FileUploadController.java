692d566e2b9d5a8484f4fd16cd0c1499c87f250b
Rushabh Jain <Rushabh.Jain@vcontractor.co.za>
2021-07-09 20:01:42 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringFileUpload/controllers/FileUploadController.java
@@ -0,0 +1,57 @@
+package com.example.SpringFileUpload.controllers;
+
+import com.example.SpringFileUpload.helpers.FileUploadHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+public class FileUploadController {
+
+    @Autowired
+    private FileUploadHelper fileUploadHelper;
+
+    @PostMapping("/upload-file")
+    public ResponseEntity<String> uploadFile(@RequestParam(value="profilePicture", required = false) MultipartFile multipartFile){
+        try{
+            System.out.println(multipartFile.getOriginalFilename());
+            System.out.println(multipartFile.getSize());
+            // 15 KB = 15219 bytes
+            // 1 KB = 1024 bytes
+            // 1 MB = 1048576 bytes
+            System.out.println(multipartFile.getContentType());
+            System.out.println(multipartFile.getName());
+
+            // Validation
+            if(multipartFile.isEmpty()){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Request must contain file");
+            }
+            else if(!multipartFile.getContentType().equals("image/jpeg")){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Only JPEG content type are allowed");
+            }
+            else if(multipartFile.getSize() > 1048579){
+                // Don't upload if image is greater than 1 MB
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Image size must not exceed 1 MB.");
+            }
+            else {
+                // File Upload
+                boolean result = fileUploadHelper.uploadFile(multipartFile);
+                if(result){
+//                    return ResponseEntity.ok("File is successfully uploaded");
+                    System.out.println("Context Path : " + ServletUriComponentsBuilder.fromCurrentContextPath().toUriString());
+                    return ResponseEntity.ok(ServletUriComponentsBuilder.fromCurrentContextPath().path("/uploads/").path(multipartFile.getOriginalFilename()).toUriString());
+                }
+            }
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong. Try again !");
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Something went wrong.");
+        }
+    }
+}
---
+++ src/main/java/com/example/SpringFileUpload/helpers/FileUploadHelper.java
@@ -0,0 +1,58 @@
+package com.example.SpringFileUpload.helpers;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.*;
+
+@Component
+public class FileUploadHelper {
+
+//     Static Path
+//    public final String UPLOAD_DIR = "C:\\Rushabh\\Projects\\IdeaProjects\\SpringFileUpload\\src\\main\\resources\\static\\uploads";
+//    public final String UPLOAD_DIR = "src\\main\\resources\\static\\uploads";
+
+//     Dynamic Path
+    // ClassPathResource will give the path of "resources" directory
+    public final String UPLOAD_DIR = new ClassPathResource("static/uploads/").getFile().getAbsolutePath();
+
+    public FileUploadHelper() throws IOException {
+
+    }
+
+    public boolean uploadFile(MultipartFile multipartFile){
+        System.out.println(UPLOAD_DIR);
+        boolean isUploaded = false;
+        try{
+//            Using IO Package
+//            // Read file
+//            InputStream inputStream = multipartFile.getInputStream();
+//            byte data[] = new byte[inputStream.available()];
+//            inputStream.read(data);
+//            inputStream.close();
+//            // Write file
+//            FileOutputStream fileOutputStream = new FileOutputStream(UPLOAD_DIR + File.separator + multipartFile.getOriginalFilename());
+//            fileOutputStream.write(data);
+//            fileOutputStream.flush();
+//            fileOutputStream.close();
+
+//            Using NIO Package
+//            public static long copy(InputStream in, Path target, CopyOption... options)
+            Files.copy(multipartFile.getInputStream(), Paths.get(UPLOAD_DIR + File.separator + multipartFile.getOriginalFilename()), StandardCopyOption.REPLACE_EXISTING);
+
+            // Set uploaded to true
+            isUploaded = true;
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            isUploaded = false;
+        }
+        return isUploaded;
+    }
+
+}
